36af6ba2f68394d73e18b7c30560f5238d53ddf5
zqarrester <30811736+zqarrester@users.noreply.github.com>
2020-09-12 16:07:44 +0700

    Add files via upload

---
+++ taptitans.ino
@@ -0,0 +1,18 @@
+#include <Servo.h> 
+Servo servo1; 
+void setup() 
+    { 
+servo1.attach(9);       servo1.write(125);
+    delay(5000); 
+servo1.write(90); 
+    delay(200); 
+servo1.write(125); 
+    delay(750);
+    } 
+void loop() 
+{ 
+servo1.write(100);
+ delay(100);
+servo1.write(100);
+ delay(100); 
+}
